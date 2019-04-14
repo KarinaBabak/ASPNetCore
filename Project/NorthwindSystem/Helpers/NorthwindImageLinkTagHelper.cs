faf38d812c6aa80026895df85f729bbe2f4b1b42
KarinaBabak <karisha.babak@gmail.com>
2019-04-15 01:01:15 +0300

    add Tag Helper

---
+++ Project/NorthwindSystem/Helpers/NorthwindImageLinkTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+
+namespace NorthwindSystem.Helpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-image-id")]
+    public class NorthwindImageLinkTagHelper : TagHelper
+    {
+        public int NorthwindImageId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("href", $"/images/{NorthwindImageId}");
+        }
+    }
+}
