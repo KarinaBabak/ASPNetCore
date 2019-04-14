fd4987841caad075e5d5414f600df9b23158314e
KarinaBabak <karisha.babak@gmail.com>
2019-04-15 01:22:22 +0300

    add Html Helper

---
+++ Project/NorthwindSystem/Helpers/NorthwindImageLinkHtmlHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace NorthwindSystem.Helpers
+{
+    public static class NorthwindImageLinkHtmlHelper
+    {
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper helper, int imageId, string linkText)
+        {
+            return new HtmlString($"<a href='/images/{imageId}'>{linkText}</a>");
+        }
+    }
+}
