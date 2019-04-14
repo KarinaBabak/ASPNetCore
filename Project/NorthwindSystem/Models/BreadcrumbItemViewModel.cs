ddcf75a24fcecf0e76a834bc8a1c98fc7d76cb21
KarinaBabak <karisha.babak@gmail.com>
2019-04-15 00:35:22 +0300

    add sections (task1), implement breadcrumbs (task2)

---
+++ Project/NorthwindSystem/Models/BreadcrumbItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace NorthwindSystem.Models
+{
+    public class BreadcrumbItemViewModel
+    {
+        public string ControllerName { get; set; }
+        public string Title { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ Project/NorthwindSystem/ViewComponents/BreadcrumbsViewComponent.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindSystem.Models;
+
+
+namespace NorthwindSystem.ViewComponents
+{
+    public class Breadcrumbs : ViewComponent
+    {
+        private readonly string _indexActionName = "Index";
+        public IViewComponentResult Invoke()
+        {
+            var currentBreadcrumbs = GetCurrentBreadcrumbTrail();
+            return View(currentBreadcrumbs);
+        }
+
+        private IEnumerable<BreadcrumbItemViewModel> GetCurrentBreadcrumbTrail()
+        {
+            var breadcrumbs = new List<BreadcrumbItemViewModel>();
+            var controller = ViewContext.RouteData.Values["controller"].ToString();
+            var action = ViewContext.RouteData.Values["action"].ToString();
+
+            if (controller.Equals("Home"))
+            {
+                return breadcrumbs;
+            }
+
+            breadcrumbs.Add(
+                new BreadcrumbItemViewModel()
+                {
+                    ControllerName = controller,
+                    Title = controller,
+                    Path = _indexActionName,
+                });
+
+            if (!action.Equals(_indexActionName))
+            {
+                breadcrumbs.Add(new BreadcrumbItemViewModel()
+                {
+                    ControllerName = controller,
+                    Title = action,
+                    Path = $"{HttpContext.Request.Path}{HttpContext.Request.QueryString}",
+                });
+            }
+
+            return breadcrumbs;
+        }
+    }
+}
